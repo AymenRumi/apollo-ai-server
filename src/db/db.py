bc93c971cb24092e507968a4366f97d38046cce3
AymenRumi <aymen.rumi@mail.mcgill.ca>
2024-02-18 13:43:24 -0500

    create db

--- src/db/db.py
+++ src/db/db.py
@@ -1,3 +1,4 @@
+from models.base import Base
 from sqlalchemy import create_engine
 
 from ..settings.db import SQLiteSettings
@@ -5,3 +6,7 @@ from ..settings.db import SQLiteSettings
 engine = create_engine(
     SQLiteSettings().database_url, connect_args={"check_same_thread": False}
 )
+
+
+def create_db() -> None:
+    Base.metadata.create_all(bind=engine)
