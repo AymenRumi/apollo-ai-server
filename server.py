0e90aaadc8d650bba9955ac645278bab90aa31a1
AymenRumi <aymen.rumi@mail.mcgill.ca>
2024-02-18 14:09:01 -0500

    using lifespan

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -5,13 +7,14 @@ from scripts import available_port
 from src.db import create_db
 from src.routes.db import db_api
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def app_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db()
+    yield
 
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(db_api)
 
